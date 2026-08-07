3c5826f220f136625896efc10ec2e5c503590a0d
DYattaw5 <zh4yvpvtd8@privaterelay.appleid.com>
2026-08-07 15:47:07 -0400

    Create app.ts

---
+++ apps/api/src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+
+import routes from "./routes/index.js";
+
+const app = express();
+
+app.use(helmet());
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(morgan("dev"));
+
+app.use("/", routes);
+
+export default app;
